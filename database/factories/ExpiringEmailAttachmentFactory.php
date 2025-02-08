2683051153eb7ad8a5a804eeb2601a2a12d43467
Casper Boone <mail@casperboone.nl>
2025-02-08 23:07:19 +0100

    Upgrade for new PHP and Laravel

--- database/factories/ExpiringEmailAttachmentFactory.php
+++ database/factories/ExpiringEmailAttachmentFactory.php
@@ -12,12 +12,12 @@ class ExpiringEmailAttachmentFactory extends Factory
 
     public function definition(): array
     {
-        $file = $this->faker->image();
+        $temporaryFile = __DIR__ . '/../../tests/resources/hugo-ruiz-e2pVrE1PYzs-unsplash.jpeg';
 
         return [
             'expiring_email_id' => ExpiringEmail::factory(),
-            'contents' => file_get_contents($file),
-            'filename' => basename($file),
+            'contents' => file_get_contents($temporaryFile),
+            'filename' => basename($temporaryFile),
             'mime_type' => 'image/png',
         ];
     }
--- src/ExpiringEmailChannel.php
+++ src/ExpiringEmailChannel.php
@@ -13,6 +13,7 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Mail;
+use Symfony\Component\Mime\Part\DataPart;
 
 class ExpiringEmailChannel
 {
@@ -32,7 +33,7 @@ class ExpiringEmailChannel
         $originalMail = $notification->toMail($notifiable);
         $recipient = $notifiable->routeNotificationFor('mail', $notification);
 
-        if (! $recipient) {
+        if (!$recipient) {
             throw InvalidEmailException::forEmail($recipient);
         }
 
@@ -64,7 +65,7 @@ class ExpiringEmailChannel
 
     private function getAttachments(MailMessage $originalMail): Collection
     {
-        $temporaryMessage = new Message(new \Swift_Message);
+        $temporaryMessage = new Message(new \Symfony\Component\Mime\Email);
 
         foreach ($originalMail->attachments as $attachment) {
             $temporaryMessage->attach($attachment['file'], $attachment['options']);
@@ -74,11 +75,10 @@ class ExpiringEmailChannel
             $temporaryMessage->attachData($attachment['data'], $attachment['name'], $attachment['options']);
         }
 
-        return collect($temporaryMessage->getSwiftMessage()->getChildren())
-            ->filter(fn ($child) => $child instanceof \Swift_Attachment)
-            ->map(fn (\Swift_Attachment $attachment) => ExpiringEmailAttachment::make([
+        return collect($temporaryMessage->getAttachments())
+            ->map(fn(DataPart $attachment) => ExpiringEmailAttachment::make([
                 'filename' => $attachment->getFilename(),
-                'mime_type' => $attachment->getBodyContentType(),
+                'mime_type' => $attachment->getContentType(),
                 'contents' => $attachment->getBody(),
             ]));
     }
